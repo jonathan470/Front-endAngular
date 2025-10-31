070533309a89c1bb4fcef9aa39c4bd0aa4292397
jonathan470 <jonarosas888@gmail.com>
2025-10-30 19:37:50 -0500

    proyecto

--- login_project/src/app/app.routes.ts
+++ login_project/src/app/app.routes.ts
@@ -1,3 +1,27 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { JonathanRosas } from './components/jonathan-rosas/jonathan-rosas';
+import { GerssonValencia } from './components/gersson-valencia/gersson-valencia';
+import { AndresDiaz } from './components/andres-diaz/andres-diaz';
+import { AndresHurtado } from './components/andres-hurtado/andres-hurtado';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'Jonathan',
+        component: JonathanRosas
+    },
+    {
+        path: 'Gersson',
+        component: GerssonValencia
+    },
+    {
+        path: 'Andres Diaz',
+        component: AndresDiaz
+    },
+    {
+        path: 'Andres Hurtado',
+        component: AndresHurtado
+    }
+
+
+];
